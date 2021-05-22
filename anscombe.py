757db140d5ade8c8457196d8c7ee12aa7540f4a1
ikcdv <ikarpicka@edu.cdv.pl>
2021-05-22 11:51:57 +0200

    add docstring

--- anscombe.py
+++ anscombe.py
@@ -8,6 +8,7 @@ import os
 
 save_results = os.path.join(os.getcwd(), "results")
 
+
 x = [10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5]
 y1 = [8.04, 6.95, 7.58, 8.81, 8.33, 9.96, 7.24, 4.26, 10.84, 4.82, 5.68]
 y2 = [9.14, 8.14, 8.74, 8.77, 9.26, 8.10, 6.13, 3.10, 9.13, 7.26, 4.74]
@@ -26,6 +27,15 @@ anscombe_df = pd.DataFrame(df_dict, index=x)
 
 
 def calculate_basic(data):
+    """This function calculate mean, standard deviation, variation,
+    correlation and indicates the max and min values
+
+    Parameters:
+    data y1, y2, y3, y4
+
+    Returns:
+    pd.DataFrame with calculations"""
+
     basic = data.describe().loc[['mean', 'std', 'max', 'min']]
     variation = pd.DataFrame({'variation': data.var()})
     correlation = data.corr()
@@ -51,11 +61,13 @@ axs[0, 1].scatter(x, y2)
 axs[1, 0].scatter(x, y3)
 axs[1, 1].scatter(x4, y4)
 
+
 if __name__ == "__main__":
     try:
         os.mkdir(save_results)
     except:
         pass
+
 
 calculate_basic(anscombe_df).to_csv(
     os.path.join(save_results, "calculate.csv"))
